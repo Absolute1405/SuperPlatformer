1d3cb4366b4b25eba295304c786641aa05e0cc35
Ivan Grigorev <absolute1405@gmail.com>
2021-10-12 18:06:34 +0300

    Pendulum trap rename

--- Assets/Traps/Sources/MovingTouchTrap.cs
+++ Assets/Traps/Sources/MovingTouchTrap.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PendulumTrap : Trap
+public class MovingTouchTrap : Trap
 {
     [SerializeField] private TrapTouchCollider _collision;
 
