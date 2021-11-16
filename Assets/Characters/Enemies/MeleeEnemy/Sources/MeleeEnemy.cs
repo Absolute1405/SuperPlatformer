74aeeda36463bb48f096f73bb58ea3460df3789c
And7412 <90712704+And7412@users.noreply.github.com>
2021-11-16 17:31:56 +0300

    HomeWork

--- Assets/Characters/Enemies/MeleeEnemy/Sources/MeleeEnemy.cs
+++ Assets/Characters/Enemies/MeleeEnemy/Sources/MeleeEnemy.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 
 public class MeleeEnemy : Enemy
 {
+    private Transform _BTransform;
+    private Vector2 _vector2Ride;
+    private Vector2 _vector2Left;
     public override void Initialize(EnemyConfig config)
     {
         base.Initialize(config);
+        
         BanditConfig banditConfig = config as BanditConfig;
+        _vector2Ride =new Vector2(_BTransform.position.x, _BTransform.position.y) + new Vector2( banditConfig.MoveRange,0);
+        _vector2Ride = new Vector2(_BTransform.position.x, _BTransform.position.y) - new Vector2(banditConfig.MoveRange, 0);
     }
 
     protected override IEnumerator LifeCycle()
     {
         while (true)
         {
+            _BTransform.Translate(_vector2Ride);
             yield return new WaitForSeconds(1f);
-            
+            _BTransform.Translate(_vector2Left);
         }
     }
 }
