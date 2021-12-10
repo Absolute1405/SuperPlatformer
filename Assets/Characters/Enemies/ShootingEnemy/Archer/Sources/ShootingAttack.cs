2998f6af592e293341ee953cda16f4ac98f3bd41
And7412 <90712704+And7412@users.noreply.github.com>
2021-12-10 18:26:15 +0300

    Update Bug

--- Assets/Characters/Enemies/ShootingEnemy/Archer/Sources/ShootingAttack.cs
+++ Assets/Characters/Enemies/ShootingEnemy/Archer/Sources/ShootingAttack.cs
@@ -6,18 +6,24 @@ public class ShootingAttack : EnemyAttack
 {
     [SerializeField] private Arrow _prefab;
     private ICharacterDirection _direction;
+    private ShootingEnemy _Archer;
+    private float _SizeBoxCollider2D;
 
     public override void Initialize(int damage)
     {
         base.Initialize(damage);
+        
         _direction = GetComponent<ICharacterDirection>();
     }
+
 
     public override void Attack(IDamageable target)
     {
         base.Attack(target);
+        _SizeBoxCollider2D = _Archer.PassArgument;
         var arrow = Instantiate(_prefab);
-        arrow.transform.position += (Vector3) DirectionGetter.GetVectorFromDirection(_direction.Value) * 3f;
+        arrow.transform.position +=  (Vector3)DirectionGetter.GetVectorFromDirection(_direction.Value) * 3f;
+        arrow.transform.position +=new Vector3(_SizeBoxCollider2D,0,0);
         _prefab.Initialaze(_direction.Value);
     }
 }
--- Assets/Characters/Enemies/ShootingEnemy/Archer/Sources/ShootingEnemy.cs
+++ Assets/Characters/Enemies/ShootingEnemy/Archer/Sources/ShootingEnemy.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
+using System;
 using UnityEngine;
 
 public class ShootingEnemy : Enemy
 {
     [SerializeField] private BoxCollider2D _rangeCollider;
-
+    public float PassArgument { get; private set; } 
     private float _delay;
     public override void Initialize(EnemyConfig config)
     {
         base.Initialize(config);
+        PassArgument = _rangeCollider.size.x;
         ArcherConfig archerConfig = config as ArcherConfig;
         _delay = archerConfig.AttackDelay;
         //_rangeCollider.size = Vector2.right * archerConfig.AttackRange;
