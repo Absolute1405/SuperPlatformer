0dc43e1c74939239e9b4fcbd0f98cfb74eb25351
And7412 <90712704+And7412@users.noreply.github.com>
2022-01-04 16:58:15 +0300

    Homework not completed yet

---
+++ Assets/Sources/Cutscene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Cutscene : MonoBehaviour
+{
+    protected Vector2[] points;
+
+    protected abstract IEnumerable LiveSaicel();
+}
