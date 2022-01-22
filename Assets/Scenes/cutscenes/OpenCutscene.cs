3a0bec745552f43df2f4f6b69ba29836a6eec928
And7412 <90712704+And7412@users.noreply.github.com>
2022-01-22 15:07:19 +0300

    Update OpenCutscene.cs

--- Assets/Scenes/cutscenes/OpenCutscene.cs
+++ Assets/Scenes/cutscenes/OpenCutscene.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class OpenCutscene : Cutscene
 {
+    [SerializeField] private float speed = 1f;
+    private Vector2 vector2 ;
     protected override IEnumerable LiveSaicel()
     {
+        foreach(Vector2 point in points)
+        {
+        //    vector2 = new Vector2(transform.position.x, transform.position.y);
+        //    Vector2 point_vctor = new Vector2(point.x, point.y);
+        //    vector2 = vector2.MoveTowards(vector2,point_vctor,speed*Time.deltaTime)
+        }
         yield return null;
     }
 }
