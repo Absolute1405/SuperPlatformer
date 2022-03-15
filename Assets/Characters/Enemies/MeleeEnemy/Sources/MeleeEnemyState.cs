ac81930b2013cb02ca5530357e341e6e031324e8
And7412 <90712704+And7412@users.noreply.github.com>
2022-03-15 16:23:09 +0300

    MeleeEnemyState and StateMachineMeleeEnemy

---
+++ Assets/Characters/Enemies/MeleeEnemy/Sources/MeleeEnemyState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MeleeEnemyState
+{
+    public List<MeleeEnemyState> PossibleNextStates { get; private set; }
+    protected EnemyAnimator animator { get; private set; }
+
+    protected MeleeEnemyState(EnemyAnimator animator)
+    {
+        PossibleNextStates = new List<MeleeEnemyState>();
+        this.animator = animator;
+    }
+
+    public abstract void OnMelleEnemyStateEnter();
+    public abstract void OnMelleEnemyStateExit();
+
+    public void AddPossibleNextStates(params MeleeEnemyState[] states)
+    {
+        PossibleNextStates.AddRange(states);
+    }
+}
---
+++ Assets/Characters/Enemies/MeleeEnemy/Sources/StateMachineMeleeEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachineMeleeEnemy
+{
+    private MeleeEnemyState _moveState;
+    private MeleeEnemyState _attackState;
+    private MeleeEnemyState _hitState;
+    private MeleeEnemyState _deathstate;
+    public StateMachineMeleeEnemy(MeleeEnemyAnimator animator)
+    {
+
+    }
+}
