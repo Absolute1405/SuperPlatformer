c6332d2341b70069bda6ce7ab6521aca64b973ea
And7412 <90712704+And7412@users.noreply.github.com>
2022-03-29 19:09:58 +0300

    C/W

---
+++ Assets/AsyncLesson/Sources/HomeWork/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private Text _text;
+
+        public int Score { get; private set} = 0;
+        private const int clickCost=25;
+        private const int delayConst = 1000;
+
+        public async Task Play(float time)
+        {
+            _button.onClick.AddListener(OnClick);
+            var ms = delayConst * time;
+            await Task.Delay((int)ms);
+            _button.onClick.RemoveListener(OnClick);
+        }
+        private void OnClick()
+        {
+            Score += clickCost;
+            _text.text = "Score " + Score.ToString();
+        }
+    }
+}
+
---
+++ Assets/AsyncLesson/Sources/HomeWork/PlayerTurns.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Game
+{
+    public class PlayerTurns : MonoBehaviour
+    {
+        [SerializeField] private Player player1;
+        [SerializeField] private Player player2;
+        [SerializeField] private int _winScore;
+        [SerializeField] private float _turnsDyreshen;
+
+        public async void  PlayInOrder()
+        {
+            while (true)
+            {
+                player1.Play(_turnsDyreshen);
+                if (player1.Score == _winScore)
+                {
+                    OnFinish(player1);
+                }
+                await
+            }
+            
+        }
+        private void OnFinish(Player player) 
+        {
+            
+        }
+    }
+}
+
