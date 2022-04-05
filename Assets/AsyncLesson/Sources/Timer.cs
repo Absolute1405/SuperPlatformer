929146a884f2c15970456fb862f05ebde706d303
And7412 <90712704+And7412@users.noreply.github.com>
2022-04-05 17:46:09 +0300

    h/w

--- Assets/AsyncLesson/Sources/Timer.cs
+++ Assets/AsyncLesson/Sources/Timer.cs
@@ -8,23 +8,29 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     [SerializeField] private Text _timerView;
-    private float _timer;
+    private float _duration;
 
-    private void Update()
-    {
-        _timer -= Time.deltaTime;
-        _timerView.text = _timer.ToString("F1");
+    public async void Setup(float _duration)
+    { 
+        for(float i=0; i <= 0; i = _duration)
+        {
+            _duration -= Time.deltaTime;
+            _timerView.text = _duration.ToString("F1");
+
+            await Task.Run(() => Stop());
+            Debug.Log("Остановка Выполнена");
+        }
     }
 
-    public void Setup(float duration)
+    public void TimerUpdate(float Valu)
     {
-        _timer = duration;
-        enabled = true;
-    }
+        
+    }                  
 
     public void Stop()
     {
-        enabled = false;
+        Debug.Log("Остановка Выполнена?");
+
     }
 
 }
