0ffbf6b9aa60d49ac481c357f73fee8021051287
Ivan Grigorev <absolute1405@gmail.com>
2022-04-16 17:32:20 +0300

    AnimatorBehaviour lesson

---
+++ Assets/AnimationsLesson/Sources/AnimationsTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AnimationsTest : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private string _leftTrigger = "MoveLeft";
+    [SerializeField] private string _rightTrigger = "MoveRight";
+
+
+    private void Start()
+    {
+        var behaviours = _animator.GetBehaviours<MoveAnimatorBehaviour>();
+
+        foreach (var behaviour in behaviours)
+        {
+            behaviour.StateEnded += BehaviourOnStateEnded;
+        }
+
+        Animate();
+        _animator.GetCurrentAnimatorStateInfo(0);
+    }
+
+    private void BehaviourOnStateEnded(string name)
+    {
+        Debug.Log(name);
+    }
+
+    private async void Animate()
+    {
+        await Task.Delay(2000);
+        _animator.SetTrigger(_leftTrigger);
+        await Task.Delay(2000);
+        _animator.SetTrigger(_rightTrigger);
+    }
+
+    private async void OnClipStarted(float lenght)
+    {
+        var time = lenght * 1000;
+        await Task.Delay((int) time);
+    }
+}
---
+++ Assets/MoveAnimatorBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnimatorBehaviour : StateMachineBehaviour
+{
+    [SerializeField] private string _name;
+    public event Action<string> StateEnded;
+    public event Action<float> StateEntered;
+
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        StateEntered?.Invoke(stateInfo.length);
+    }
+
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        StateEnded?.Invoke(_name);
+    }
+
+}
