ba7ec69a776756a7ae161b0a7406382dd019e645
Ivan Grigorev <absolute1405@gmail.com>
2022-05-21 17:35:50 +0300

    Rigidbody explained

--- Assets/StateMachineLesson/Scripts/PlayerJump.cs
+++ Assets/StateMachineLesson/Scripts/PlayerJump.cs
@@ -9,46 +9,53 @@ namespace AppleGame
     public class PlayerJump : MonoBehaviour
     {
         [SerializeField] private float _jumpForce = 10f;
-        [SerializeField] private float _maxJump = 15f;
-        [SerializeField] private const float _G= 9.81f;
+        [SerializeField] private float _gravityForce = -9.81f;
+        [SerializeField] private float _gravityScale = 5f;
+
+        public float _velocity = 0;
+
         private Rigidbody2D _rigidbody;
         public event Action<bool> OnAir;
+        private bool _onAir;
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
         }
 
+        private void Update()
+        {
+            var pos = transform.position;
+            var y = pos.y;
+            var destination = y + _velocity * Time.deltaTime / 2;
+            transform.position = new Vector3(pos.x, destination);
+
+            if (_velocity > _gravityForce && _onAir)
+            {
+                _velocity -= Time.deltaTime * _gravityScale;
+            }
+        }
+        
+
         private void OnCollisionEnter2D(Collision2D collision)
         {
             OnAir?.Invoke(false);
+            _velocity = 0;
+            _onAir = false;
         }
+
         private void OnCollisionExit2D(Collision2D collision)
         {
-
             OnAir?.Invoke(true);
+            _onAir = true;
         }
 
-        public async void Jump()
+        public void Jump()
         {
-            
             var force = Vector2.up * _jumpForce;
             //_rigidbody.AddForce(force, ForceMode2D.Impulse);
-            var maxJumpValiu = _maxJump + transform.position.y;
-            var startpositionY = transform.position.y;
-            for (float positionY = transform.position.y; positionY<maxJumpValiu; positionY = transform.position.y)
-            {
-                transform.Translate(new Vector2(0,_jumpForce*Time.deltaTime));
-                await Task.Yield();
-            }
-            for (float positionY = transform.position.y; positionY > startpositionY; positionY = transform.position.y)
-            {
-                transform.Translate(new Vector2(0,  -(_G* Time.deltaTime)));
-                await Task.Yield();
-            }
-
+            _velocity += _jumpForce;
         }
-
     }
 }
 
